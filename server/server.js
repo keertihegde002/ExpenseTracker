317adb250b340fa0acd81e7e1c6f505fa32384a9
keerti <keertihegde002@gmail.com>
2025-08-01 21:52:49 +0530

    updated port

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.use("/api/v1/dashboard", dashboardRoutes);
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
